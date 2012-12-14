4e5c7c22b218e9f560556c7f1c099e69e047e4bb
Michael Pollmeier <michael@movio.co>
2012-12-14 03:55:56 +0000

    showcasing implicits

--- src/test/scala/examples/ScalaLangExamples.scala
+++ src/test/scala/examples/ScalaLangExamples.scala
@@ -5,141 +5,150 @@ import org.scalatest.junit.JUnitRunner
 import org.scalatest.FunSuite
 import scala.beans.BeanProperty
 import scala.tools.util.Javap
+import org.scalatest.matchers.ShouldMatchers
 
 @RunWith(classOf[JUnitRunner])
-class ScalaLangExamples extends FunSuite {
+class ScalaLangExamples extends FunSuite with ShouldMatchers {
 
-  test("variables") {
-    var variable = "var" // string var, mutable - try not to use
-    val value = "one " + "two" // string val - evaluated once
-    lazy val lazie = "one " + "two" // only evaluated when needed but only once (be careful tho)
-    def function = "one " + "two" // evaluated every time it's called
-  }
+	test("variables") {
+		var variable = "var" // string var, mutable - try not to use
+		val value = "one " + "two" // string val - evaluated once
+		lazy val lazie = "one " + "two" // only evaluated when needed but only once (be careful tho)
+		def function = "one " + "two" // evaluated every time it's called
+	}
 
-  test("functions") {
-    def coolMethod(count: Int = 0, name: String = "default") = s"""count:$count name:$name"""
-    println(coolMethod())
-    println(coolMethod(10, "two"))
-    println(coolMethod(name = "just a string"))
-  }
+	test("functions") {
+		def coolMethod(count: Int = 0, name: String = "default") = s"""count:$count name:$name"""
+		println(coolMethod())
+		println(coolMethod(10, "two"))
+		println(coolMethod(name = "just a string"))
+	}
 
-  test("optional examples") {
-    // Option
-    var badVar: String = null
-    var goodVar: Option[String] = None
+	test("optional examples") {
+		// Option
+		var badVar: String = null
+		var goodVar: Option[String] = None
 
-    // Using Optional
-    goodVar match {
-      case Some(value) => { println(value) }
-      case None => { println("not found") }
-    }
+		// Using Optional
+		goodVar match {
+			case Some(value) => { println(value) }
+			case None => { println("not found") }
+		}
 
-    // Other ways
-    val usableVar = goodVar.getOrElse("not found")
-    println(usableVar)
+		// Other ways
+		val usableVar = goodVar.getOrElse("not found")
+		println(usableVar)
 
-    // awesome way
-    val user: Option[String] = Some("kal")
-    val pass: Option[String] = None
-    def validUser(user: String, pass: String) = user == pass
-    for {
-      u <- user // continue if found
-      p <- pass // continue if found
-      if validUser(u, p) // continue if valid
-    } println(s"""user:$u logged in""")
+		// awesome way
+		val user: Option[String] = Some("kal")
+		val pass: Option[String] = None
+		def validUser(user: String, pass: String) = user == pass
+		for {
+			u <- user // continue if found
+			p <- pass // continue if found
+			if validUser(u, p) // continue if valid
+		} println(s"""user:$u logged in""")
 
-  }
+	}
 
-  test("block examples") {
-    // Bad if block
-    var badResult = ""
-    if (1 == 1)
-      badResult = "good"
-    else
-      badResult = "bad"
-    println(badResult)
+	test("block examples") {
+		// Bad if block
+		var badResult = ""
+		if (1 == 1)
+			badResult = "good"
+		else
+			badResult = "bad"
+		println(badResult)
 
-    // Better (no vars)
-    val goodResult = if (1 == 1)
-      "good"
-    else
-      "bad"
-    println(goodResult)
+		// Better (no vars)
+		val goodResult = if (1 == 1)
+			"good"
+		else
+			"bad"
+		println(goodResult)
 
-    /** TRY Blocks */
-    var badTry = ""
-    try {
-      // try to read file
-      badTry = "..."
-    } catch {
-      case e => e.printStackTrace
-    }
-    println(badTry)
+		/** TRY Blocks */
+		var badTry = ""
+		try {
+			// try to read file
+			badTry = "..."
+		} catch {
+			case e => e.printStackTrace
+		}
+		println(badTry)
 
-    val goodTry = try {
-      "..."
-    } catch {
-      case e: Throwable => e.printStackTrace
-    }
-    println(goodTry)
-  }
+		val goodTry = try {
+			"..."
+		} catch {
+			case e: Throwable => e.printStackTrace
+		}
+		println(goodTry)
+	}
 
-  // just an example, this would be a bad idea
-  // see also scala.collections.JavaConversions._
-  test("implicit conversions") {
-    implicit def javaIntToScalaString(in: Integer): String = in.toString()
-    val javaVersion = new Integer(123)
+	test("entities") {
+		/**
+		 * Java Example
+		 *
+		 * public class Person {
+		 *   private String firstName;
+		 *   private String lastName;
+		 *
+		 * public Person(String firstName, String lastName) {
+		 *   this.firstName = firstName;
+		 *   this.lastName = lastName;
+		 * }
+		 *
+		 * public String getFirstName() {
+		 *   return firstName;
+		 * }
+		 *
+		 * public void setFirstName(String firstName) {
+		 *   this.firstName = firstName;
+		 * }
+		 *
+		 * public String getLastName() {
+		 *   return lastName;
+		 * }
+		 *
+		 * public void setLastName(String lastName) {
+		 *   this.lastName = lastName;
+		 * }
+		 * }
+		 *
+		 */
+		class JavaPerson(
+			@BeanProperty var firstName: String,
+			@BeanProperty var lastName: String)
 
-    def onlyTakesInts(in: String) {}
-    onlyTakesInts(javaVersion) // automagically converted
-  }
+		val javaPerson = new JavaPerson("kal", "bek")
+		javaPerson.setFirstName("updated")
+		println(javaPerson.getFirstName)
 
-  test("entities") {
-    /**
-     * Java Example
-     *
-     * public class Person {
-     *   private String firstName;
-     *   private String lastName;
-     *
-     * public Person(String firstName, String lastName) {
-     *   this.firstName = firstName;
-     *   this.lastName = lastName;
-     * }
-     *
-     * public String getFirstName() {
-     *   return firstName;
-     * }
-     *
-     * public void setFirstName(String firstName) {
-     *   this.firstName = firstName;
-     * }
-     *
-     * public String getLastName() {
-     *   return lastName;
-     * }
-     *
-     * public void setLastName(String lastName) {
-     *   this.lastName = lastName;
-     * }
-     * }
-     *
-     */
-    class JavaPerson(
-      @BeanProperty var firstName: String,
-      @BeanProperty var lastName: String)
+		class ScalaPerson(var firstName: String, var lastName: String)
+		val scalaPerson = new ScalaPerson("kal", "bek")
+		scalaPerson.firstName = "updated"
+		println(scalaPerson.firstName)
 
-    val javaPerson = new JavaPerson("kal", "bek")
-    javaPerson.setFirstName("updated")
-    println(javaPerson.getFirstName)
+		// Better (val's not vars)(case class)
+		case class ScalaPersonVals(val firstName: String, val lastName: String)
+		println(ScalaPersonVals("kal", "bek"))
+	}
 
-    class ScalaPerson(var firstName: String, var lastName: String)
-    val scalaPerson = new ScalaPerson("kal", "bek")
-    scalaPerson.firstName = "updated"
-    println(scalaPerson.firstName)
+	test("implicit method parameters") {
+		case class Context
+		def ask(question: String)(implicit context: Context) = 42
 
-    // Better (val's not vars)(case class)
-    case class ScalaPersonVals(val firstName: String, val lastName: String)
-    println(ScalaPersonVals("kal", "bek"))
-  }
+		implicit val context = Context()
+		ask("my question") should be(42)
+	}
+
+	// just an example, this would be a bad idea
+	// see also scala.collections.JavaConversions._
+	test("implicit conversions") {
+		implicit def javaIntToScalaString(in: Integer): String = in.toString()
+		val javaInteger = new Integer(123)
+
+		def onlyTakesStrings(in: String) {}
+		onlyTakesStrings(javaInteger) // automagically converted
+	}
 }
